19296c9dfe6fecfa8337e65d2620639a3f8c577e
drjakostein97 <jacquesflores97@gmail.com>
2026-08-30 21:01:18 -0400

    Adds the box sprites and character

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
 public class LevelManager : MonoBehaviour
 {
     public TileMapper tileMapper;
+    public Tilemap referenceTilemap;
+    public GameObject playerPrefab;
+    public GameObject boxPrefab;
+    public Sprite[] boxSprites;
 
     private SokobanGrid grid;
     private GameState state;
+
+    private GameObject playerInstance;
+    private List<GameObject> boxInstances = new();
 
     void Start()
     {
@@ -13,5 +22,29 @@ public class LevelManager : MonoBehaviour
         (grid, state) = LevelParser.ParseLevel(levelText);
 
         tileMapper.RenderGrid(grid);
+        SpawnEntities();
+    }
+
+    void SpawnEntities()
+    {
+        Vector3 playerWorldPos = referenceTilemap.CellToWorld(new Vector3Int(state.playerPos.x, state.playerPos.y, 0));
+        playerWorldPos += referenceTilemap.cellSize / 2f;
+        playerInstance = Instantiate(playerPrefab, playerWorldPos, Quaternion.identity);
+
+        foreach (var boxPos in state.boxPositions)
+        {
+            Vector3 boxWorldPos = referenceTilemap.CellToWorld(new Vector3Int(boxPos.x, boxPos.y, 0));
+            boxWorldPos += referenceTilemap.cellSize / 2f;
+
+            var boxObj = Instantiate(boxPrefab, boxWorldPos, Quaternion.identity);
+
+            if (boxSprites != null && boxSprites.Length > 0)
+            {
+                var sr = boxObj.GetComponent<SpriteRenderer>();
+                sr.sprite = boxSprites[Random.Range(0, boxSprites.Length)];
+            }
+
+            boxInstances.Add(boxObj);
+        }
     }
 }
